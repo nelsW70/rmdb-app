8611754ac379ab521c28cb44a94daea917f2e687
nelsW70 <nelsfoltz@gmail.com>
2018-12-31 17:08:26 -0500

    first commit

---
+++ src/config.js
@@ -0,0 +1,28 @@
+// Configuration for TMDB
+// To see the latest configuration fetch it from https://api.themoviedb.org/3/configuration?api_key=019e8f375549e0bbd4a4191862ebc88f
+
+const API_URL = "https://api.themoviedb.org/3/";
+// sample key
+// const API_KEY = "844dba0bfd8f3a4f3799f6130ef9e335";
+
+const API_KEY = "0002ce4aa2cfe1f35aebbe60d629bd6b";
+
+// Images
+// An image URL looks like this example:
+// http://image.tmdb.org/t/p/w780/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg
+
+const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
+
+//Sizes: w300, w780, w1280, original
+const BACKDROP_SIZE = "w1280";
+
+// w92, w154, w185, w342, w500, w780, original
+const POSTER_SIZE = "w500";
+
+export { API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE };
+
+// my key
+// 0002ce4aa2cfe1f35aebbe60d629bd6b
+
+// my read access token
+// eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMDAyY2U0YWEyY2ZlMWYzNWFlYmJlNjBkNjI5YmQ2YiIsInN1YiI6IjVjMjdmMmUyMGUwYTI2NTQxYzM1MDQ5NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qL2Za9-PuZdfJgN1Q9YBAjeQuE2IxCtqdkPRgzv2ZqA
